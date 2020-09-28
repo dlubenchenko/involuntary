66075cd6af4e976eb509bb52ea8f92198d45f279
dlubenchenko <d.lubenchenko@gmail.com>
2020-09-28 21:51:43 +0300

    my first commit

--- involuntary/app/js/main.js
+++ involuntary/app/js/main.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+const involDt = 'invol refund due to '
+const doi = 'DOI - '
+const tkt = 'TKT - '
+function sumAll() {
+    let allInfo = ['']
+    const formValue = 6
+    allInfo.shift()
+    for (let i = 1; i < formValue; i++) {
+        allInfo.push(document.RA[i].value)
+        console.log(allInfo)
+        document.RA[6].value = involDt 
+        + allInfo.slice(0, 2).join(' ') 
+        + ' / ' 
+        + tkt 
+        + allInfo[2]
+        + ' / '
+        + doi
+        + allInfo[3]
+        +  ' / '
+        + allInfo[4]
+    }
+}
