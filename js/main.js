9601fd30c9d020823b078d6368b7f40623b36850
dlubenchenko <d.lubenchenko@gmail.com>
2020-11-12 12:16:00 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -15,18 +15,19 @@ function sumAll() {
 
 
 
-const findTaxes = 'TAX    158280YQ TAX      7914YR TAX      4456YQ TAX      4922DE TAX     21132RA TAX     96636RA TAX      8709RA';
+const findTaxes = 'TX01 EUR    198.52YR  TX02 EUR      4.68FR  TX03 EUR     19.64QX           TX04 EUR      3.00M6  TX05 EUR     20.00TR  TX06 EUR      3.33XA           TX07 EUR      5.88XY  TX08 EUR      4.95YC  TX09 EUR      4.70AY           TX10 EUR      7.06CJ  TX11 EUR      6.93RN  TX12 EUR      3.78XF';
 
 // выделение такс
-const taxes = findTaxes.split(' ').filter((el) => {
-    return el != 'TAX' && el != ''
+const taxes = findTaxes.split(' ')
+const filteredTaxes = taxes.filter((el) => {
+    return el != '' && el.indexOf('TX') && el.indexOf(taxes[1])
   })
-// console.log(taxes)
+// console.log(filteredTaxes)
 
 // разделение такс
 let taxNum = []
 let taxSymbol = []
-taxes.forEach(el => {
+filteredTaxes.forEach(el => {
     return taxNum.push(Number(el.slice(0,-2))) && taxSymbol.push(el.slice(-2))
 });
 // console.log(taxNum)
@@ -56,6 +57,79 @@ taxNum = taxNum.filter((el) => {
     return el != ''
   })
 
-console.log(taxNum)
+console.log('Всі такси', '\n',  taxNum)
 console.log(taxSymbol)
 
+
+// FQQ
+const taxesFqq = 'EUR    11.76-YQ   XT EUR 4.68-FR EUR 19.64-QX EUR 3.00-M6 EUR   100.73-YR   EUR 20.00-TR EUR 3.33-XA EUR 5.88-XY EUR EUR    61.48-XT   4.95-YC'
+
+const filteredTaxFqq = taxesFqq.split(' ')
+const filteredTaxesFQQ = filteredTaxFqq.filter((el) => {
+    return el.indexOf(filteredTaxFqq[0]) && el != '' && el.indexOf('-') > -1 && el.slice(-2).indexOf('YQ') && el.slice(-2).indexOf('YR') && el.slice(-2).indexOf('XT')
+  })
+//   console.log(filteredTaxesFQQ)
+
+
+// разделение такс FQQ
+let taxNumFqq = []
+let taxSymbolFqq = []
+filteredTaxesFQQ.forEach(el => {
+    return taxNumFqq.push(Number(el.slice(0,-3))) && taxSymbolFqq.push(el.slice(-2))
+});
+// console.log(taxNumFqq)
+// console.log(taxSymbolFqq)
+
+// поиск похожых
+while (i < taxSymbolFqq.length) {
+    while (j <= taxSymbolFqq.length) {
+        if (taxSymbolFqq[i] === taxSymbolFqq[j]) {
+            taxNumFqq[i] += taxNumFqq[j]
+            taxNumFqq[j] = ''
+            taxSymbolFqq[j] = ''            
+        }
+        j++
+    }
+    i++
+    j = i + 1
+}
+
+// фильтр такс
+taxSymbolFqq = taxSymbolFqq.filter((el) => {
+    return el != ''
+  })
+  taxNumFqq = taxNumFqq.filter((el) => {
+    return el != ''
+  })
+
+// Вручну додати YQ YR
+taxNumFqq.push(100.73)
+taxSymbolFqq.push('YR')
+
+console.log('Всі використані такси', '\n', taxNumFqq)
+console.log(taxSymbolFqq)
+
+
+// поиск такс к возврату
+
+
+
+// поиск похожых для возврата
+let taxNumRef = []
+let taxSymbolRef = []
+
+i = 0
+j = 0
+while (i < taxSymbol.length) {
+    while (j < taxSymbolFqq.length) {
+        if (taxSymbol[i] === taxSymbolFqq[j]) {
+            taxNumRef.unshift(taxNum[i] - taxNumFqq[j])
+            taxSymbolRef.unshift(taxSymbol[i])
+        }
+        j++
+    }
+    i++
+    j = 0
+}
+console.log('Такси до повернення', '\n', taxNumRef)
+console.log(taxSymbolRef)
